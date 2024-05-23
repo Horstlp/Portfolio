dd1e386ed2135692dfe390dbd14848bdddddecb3
Horstlp <JEI220356@spengergasse.at>
2024-05-23 10:59:29 +0200

    Website

---
+++ script.js
@@ -0,0 +1,27 @@
+let modal = document.getElementById('fullscreenModal');
+let modalImg = document.getElementById('fullscreenImage');
+let images = document.getElementsByClassName('gallery-item');
+let currentIndex = 0;
+
+for (let i = 0; i < images.length; i++) {
+    images[i].onclick = function () {
+        modal.style.display = 'block';
+        modalImg.src = this.src;
+        currentIndex = i;
+    };
+}
+
+let closeModal = document.getElementsByClassName('close')[0];
+closeModal.onclick = function () {
+    modal.style.display = 'none';
+};
+
+function changeImage(direction) {
+    currentIndex += direction;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    } else if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+    modalImg.src = images[currentIndex].src;
+}
